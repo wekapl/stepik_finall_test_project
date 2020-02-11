be92ea5f56211de490350b4e8c1b4398cf30ab89
irina <ikwekapl@yandex.ru>
2020-02-11 22:02:44 +0300

    add parametrize link

--- pages/product_page.py
+++ pages/product_page.py
@@ -1,6 +1,5 @@
 import time
 
-import self as self
 from selenium.common.exceptions import NoAlertPresentException
 
 from .base_page import BasePage
@@ -8,7 +7,7 @@ from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
-    #def should_be_can_add_product_to_basket(self):
+    # def should_be_can_add_product_to_basket(self):
     #    self.should_be_message_about_product_in_basket()
     #    self.should_be_name_product_as_baskets_name_product()
     #    self.should_be_message_about_price()
@@ -19,36 +18,31 @@ class ProductPage(BasePage):
         assert self.is_element_present(*ProductPageLocators.MESSAGE_PRODUCT_NAME), \
             "No message add product to basket is not presented"
 
-    def should_be_name_product_as_baskets_name_product(self):
-        name_product = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
-        print('название продукта: ' + name_product)
-        # print('стоимость продукта в корзине: ' + self.browser.find_element(
-        # *ProductPageLocators.MESSAGE_PRODUCT_PRICE).text)
-        name_product_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_NAME).text
-        print('название продукта: в сообщении: ' + name_product_basket)
-        assert name_product_basket == name_product, \
-            "Name product not as name product in basket"
-
     def should_be_message_about_price(self):
         # сообщение о стоимости продукта в корзине
         assert self.is_element_present(*ProductPageLocators.MESSAGE_PRODUCT_PRICE), \
             "No message add product to basket is not presented"
 
-    def shout_be_price_products_as_baskets_price(self):
-        price_product = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
-        print('стоимость продукта: ' + price_product)
-        # print('стоимость продукта в корзине: ' + self.browser.find_element(
-        # *ProductPageLocators.MESSAGE_PRODUCT_PRICE).text)
-        price_product_basket = self.browser.find_element(*ProductPageLocators.MESSAGE_PRODUCT_PRICE).text
-        print('стоимость продукта в сообщении: ' + price_product_basket)
-        assert price_product_basket == price_product, \
-            "Price product not as price product in basket"
+    def name_product_in_element(self, product_locator):
+        # поиск элемента по локатору, возвращает текст_элемента
+        name = self.browser.find_element(*product_locator).text
+        print('название продукта: ' + name)
+        return name
+
+    def should_be_name_product_as_baskets_name_product(self, name):
+        # совпадает ли название продукта с продуктом в корзине
+        message_name = self.name_product_in_element(ProductPageLocators.MESSAGE_PRODUCT_NAME)
+        assert name == message_name, \
+            name + " product not as " + message_name + "product in basket"
+
+    def shout_be_price_products_as_baskets_price(self, price):
+        # совпадает ли цена продукта с ценой продукта в корзине
+        message_price = self.name_product_in_element(ProductPageLocators.MESSAGE_PRODUCT_PRICE)
+        assert price == message_price, \
+            price + " product not as " + message_price + "product in basket"
 
     def add_product_in_basket(self):
-        print('название продукта(добавляем в корзину): ' + self.browser.find_element(
-            *ProductPageLocators.PRODUCT_NAME).text)
-        print('стоимость продукта(добавляем в корзину): ' + self.browser.find_element(
-            *ProductPageLocators.PRODUCT_PRICE).text)
+        #  добавляем товар в корзину
         button_basket = self.browser.find_element(*ProductPageLocators.BASKET_BUTTON)
         button_basket.click()
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,15 +1,27 @@
+import time
+
+import pytest
+
+from .pages.locators import ProductPageLocators
 from .pages.product_page import ProductPage
 from .pages.main_page import MainPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link)
+@pytest.mark.parametrize('link', ["0", "1", "2", "3", "4", "5", "6",
+                                  pytest.param("7", marks=pytest.mark.xfail(reason="test fail")),
+                                  "8", "9"])
+def test_guest_can_add_product_to_basket(browser, link):
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    product_link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
+    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+    print(product_link)
+    product_page = ProductPage(browser, product_link)
     product_page.open()
-
     product_page.should_be_button_add_product_in_basket()
     product_page.add_product_in_basket()
     product_page.should_be_message_about_product_in_basket()
-    product_page.should_be_name_product_as_baskets_name_product()
     product_page.should_be_message_about_price()
-    product_page.shout_be_price_products_as_baskets_price()
+    product_page.should_be_name_product_as_baskets_name_product \
+        (product_page.name_product_in_element(ProductPageLocators.PRODUCT_NAME))
+    product_page.shout_be_price_products_as_baskets_price \
+        (product_page.name_product_in_element(ProductPageLocators.PRODUCT_PRICE))
